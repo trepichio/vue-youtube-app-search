90e2bd50217de7e71b143b111a7e68a504b881fc
trepichio <trepichio@gmail.com>
2019-10-09 18:46:51 -0300

    add SearchForm component

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="container">
+		<form class="mb-5">
+			<div class="input-group">
+				<input
+					type="text"
+					v-model="searchString"
+					@keydown.13.prevent="parseSearchString"
+					class="form-control"
+					placeholder="Search ..."
+				>
+				<div class="input-group-append">
+					<button
+						type="button"
+						class="btn btn-outline-secondary"
+						@click='parseSearchString'
+					>
+						<i class="fas fa-search"></i>
+					</button>
+				</div>
+			</div>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+
+  name: 'SearchForm',
+
+  data () {
+    return {
+    	searchString: ''
+    }
+  },
+  methods: {
+    parseSearchString () {
+      // Trim search String
+      const trimmedSearchString = this.searchString.trim()
+
+      if (trimmedSearchString != '') {
+      	// Split search string
+      	const searchParams = trimmedSearchString.split(/\s+/)
+      	// Emit event
+      	this.$emit('search', searchParams)
+      	// Reset input
+      	this.searchString = ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+	input,
+	button {
+		box-shadow: none !important;
+	}
+
+	.form-control {
+		border-color: #6c757d;
+	}
+</style>
