27dee695451988f656f730936e9ddc586ccf8a09
trepichio <trepichio@gmail.com>
2019-10-09 16:25:38 -0300

    add methods to construct url and make requests to API

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+
+  name: 'App',
+  components: {},
+  data () {
+    return {
+      videos: [],
+      reformatedSearchString: '',
+      api: {
+        baseUrl: 'https://www.googleapis.com/youtube/v3/search?',
+        type: 'video',
+        part: 'snippet',
+        order: 'videoCount',
+        maxResults: 12,
+        q: '',
+        key: YOUTUBE_API_KEY
+        prevPageToken: '',
+        nextPageToken: '',
+      }
+    }
+  },
+  methods: {
+    search (searchParams) {
+      this.reformatedSearchString = searchParams.join(' ')
+      this.api.q = this.searchParams.join('+')
+      const { baseUrl, type, part, order, maxResults, q, key } = this.api
+      const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&maxResults=${maxResults}&key=${key}&q=${q}`
+      this.getData(apiUrl)
+    },
+    prevPage () {
+      const { baseUrl, type, part, order, maxResults, q, key, prevPageToken } = this.api
+      const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&maxResults=${maxResults}&key=${key}&q=${q}&prevPageToken=${prevPageToken}`
+      this.getData(apiUrl)
+    },
+    nextPage () {
+      const { baseUrl, type, part, order, maxResults, q, key, nextPageToken } = this.api
+      const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&maxResults=${maxResults}&key=${key}&q=${q}&nextPageToken=${nextPageToken}`
+      this.getData(apiUrl)
+    },
+    getData (apiUrl) {
+      axios
+        .get(apiUrl)
+        .then(res => {
+          this.videos = res.data.items
+          this.api.prevPageToken = res.data.prevPageToken
+          this.api.nextPageToken = res.data.nextPageToken
+        })
+        .catch(error => console.error(error))
+    },
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
