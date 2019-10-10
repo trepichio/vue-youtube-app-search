d760537f6e0aaac25ded55ffa225575e2c44a98f
trepichio <trepichio@gmail.com>
2019-10-09 23:29:39 -0300

    add Pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="container">
+		<nav>
+			<ul class="pagination justify-content-end">
+				<li class="page-item"
+					:class="{ disabled: prevPageToken === undefined }"
+				>
+					<a href="#" class="page-link" @click="prevPage">Previous</a>
+				</li>
+				<li class="page-item"
+					:class="{ disabled: nextPageToken === undefined }"
+				>
+					<a href="#" class="page-link" @click="nextPage">Next</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+</template>
+
+<script>
+export default {
+
+  name: 'Pagination',
+  props: [ 'prevPageToken', 'nextPageToken'],
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    nextPage () {
+      this.$emit('next-page')
+    },
+    prevPage () {
+      this.$emit('prev-page')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
